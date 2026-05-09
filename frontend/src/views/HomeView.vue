a5a19cebd8530d59517563772cc2f900d7103a24
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-05-09 12:24:01 +0700

    konfigurasi tailwind css

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <main>
-    <h1>Halaman Beranda</h1>
+  <main class="min-h-screen bg-gray-50 flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-blue-600">Halaman Beranda UMKM Desa</h1>
   </main>
 </template>
