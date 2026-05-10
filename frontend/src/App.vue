59f269352d9884a69af6d45417833028463ed0a8
Ikhlas Sahputra <ikhlasamalillah568@gmail.com>
2026-05-10 09:43:45 +0700

    update app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <RouterView />
+  <div class="min-h-screen bg-white font-sans text-gray-900">
+    <Navbar />
+    <RouterView />
+    <Footer />
+  </div>
 </template>
 
 <style scoped>
